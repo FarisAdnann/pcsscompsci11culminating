933b79bfdf7fd626e945b93c3f4dc30ce9a23331
Josharis <45038547+Josharis@users.noreply.github.com>
2018-11-30 09:15:58 -0500

    Resolve conflict

--- login_form.php
+++ login_form.php
@@ -1,56 +1,46 @@
 <html>
 <style>
 body {font-family: sans-serif;} * {box-sizing: border-box;}
-input[type=submit],  {
+input[type=text], input[type=password] {
     width: 100%;
-    padding: 15x;
+    padding: 15px;
     margin: 6px 0 16px 0;
     display: inline-block;
     border: none;
     background: #f1f1f1;
 }
-.button {
-    background-color: #4CAF50; /* Green */
-    border: none;
+button {
+    background-color: green;
     color: white;
     padding: 14px 20px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
+    margin: 8px 10px;
+    border: 1px solid green;
     -webkit-transition-duration: 0.4s; /* Safari */
     transition-duration: 0.4s;
     cursor: pointer;
+    width: 8%;
 }
-.button1 {
-    background-color: green; 
-    color: black; 
-    border: 2px solid #4CAF50;
-}
-.button1:hover {
-    background-color: #4CAF50;
+button:hover{
+  background-color: #4CAF50;
     color: white;
     box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
 }    
-
 input[type=text]:focus, input[type=password]:focus {
     background-color: #ddd;
     outline: none;
 }
 .container {padding: 16px;}
-div textarea {
-    background-color: lightgrey;
-    width: 300px;
-    border: 25px solid green;
-    padding: 25px;
-    margin: 25px;
-}
-</style> 
-<form>
-<input type="text" name="message" placeholder="Type Message"></input>
-<br> 
-<button class="button button1" type="submit">Submit</button> 
-</form> 
-</html> 
-<?php ?>
+</style>
+<body>
+<form method="post" style="border:2px solid #ccc" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
+<div class="container">
+<h1> Student Login </h1> 
+<label for="username"><b>Username</b></label><input type="text" placeholder="Enter Username" name="username" required><br>
+<label for="password"><b>Password</b></label><input type="password" name="password" placeholder="Enter Password" required><br>
+<div class="clearfix">
+<button type="submit" name="login" value="Log In"><b>Log In</b>
+<button type="reset" name="erase" value="Erase"><b>Reset</b>
+</div><br>
+<a href="student_registration.php">Sign Up!</a>
+</div>
+</form>
