5885f85cf5ecbed72f7d7412c1fab2332bebe0f1
Josharis <45038547+Josharis@users.noreply.github.com>
2018-11-30 09:27:09 -0500

    Student registration

---
+++ student_registration.php
@@ -0,0 +1,47 @@
+<html>
+<style>
+body {font-family: sans-serif;} * {box-sizing: border-box;}
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 15px;
+    margin: 6px 0 16px 0;
+    display: inline-block;
+    border: none;
+    background: #f1f1f1;
+}
+button {
+    background-color: green;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 10px;
+    border: 1px solid green;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    width: 8%;
+}
+button:hover{
+
+  background-color: #4CAF50;
+    color: white;
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}    
+input[type=text]:focus, input[type=password]:focus {
+    background-color: #ddd;
+    outline: none;
+}
+.container {padding: 16px;}
+</style>
+<body>
+<form method="post" style="border:2px solid #ccc" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
+<div class="container">
+<h1> Student Registration </h1> 
+<label for="username"><b>Username</b></label><input type="text" placeholder="Enter Username" name="username" required><br>
+<label for="password"><b>Password</b></label><input type="password" name="password" placeholder="Enter Password" required><br>
+<div class="clearfix">
+<button type="submit" name="register" value="Register"><b>Register</b>
+<button type="reset" name="erase" value="Erase"><b>Reset</b>
+</div><br>
+<a href="login_form.php">Log In!</a>
+</div>
+</form>
