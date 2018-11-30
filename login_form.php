2d7d706c422e81d58cb27b79748f38578641e063
Josharis <45038547+Josharis@users.noreply.github.com>
2018-11-30 08:56:16 -0500

    Hover button transition

--- login_form.php
+++ login_form.php
@@ -1,42 +1,56 @@
 <html>
 <style>
 body {font-family: sans-serif;} * {box-sizing: border-box;}
-input[type=text], input[type=password] {
+input[type=submit],  {
     width: 100%;
-    padding: 15px;
+    padding: 15x;
     margin: 6px 0 16px 0;
     display: inline-block;
     border: none;
     background: #f1f1f1;
 }
-button {
-    background-color: #4CAF50;
+.button {
+    background-color: #4CAF50; /* Green */
+    border: none;
     color: white;
     padding: 14px 20px;
-    margin: 8px 10px;
-    border: 1px solid green;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
     cursor: pointer;
-    width: 8%;
 }
-button:hover{
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+.button1 {
+    background-color: green; 
+    color: black; 
+    border: 2px solid #4CAF50;
 }
+.button1:hover {
+    background-color: #4CAF50;
+    color: white;
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}    
+
 input[type=text]:focus, input[type=password]:focus {
     background-color: #ddd;
     outline: none;
 }
 .container {padding: 16px;}
-</style>
-<body>
-<form method="post" style="border:2px solid #ccc" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
-<div class="container">
-<h1> Student Login </h1> 
-<label for="username"><b>Username</b></label><input type="text" placeholder="Enter Username" name="username" required><br>
-<label for="password"><b>Password</b></label><input type="password" name="password" placeholder="Enter Password" required><br>
-<div class="clearfix">
-<button type="submit" name="login" value="Log In"><b>Log In</b>
-<button type="reset" name="erase" value="Erase"><b>Reset</b>
-</div><br>
-<a href="student_registration.php">Sign Up!</a>
-</div>
-</form>
+div textarea {
+    background-color: lightgrey;
+    width: 300px;
+    border: 25px solid green;
+    padding: 25px;
+    margin: 25px;
+}
+</style> 
+<form>
+<input type="text" name="message" placeholder="Type Message"></input>
+<br> 
+<button class="button button1" type="submit">Submit</button> 
+</form> 
+</html> 
+<?php ?>
