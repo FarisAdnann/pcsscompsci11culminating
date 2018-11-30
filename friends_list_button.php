e2a5aa22e5221f1f67d0266efa90b4d066d212f4
Josharis <45038547+Josharis@users.noreply.github.com>
2018-11-30 09:00:59 -0500

    Changed to php file

---
+++ friends_list_button.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<style>
+
+
+button {
+    background-color: red;
+    color: white;
+    padding: 20px 20px;
+    margin: 10px 10;
+    border: none;
+        border-radius: 17px;
+ box-shadow:  0 7px #998;
+    cursor: pointer;
+    width: 18%;
+   }
+
+button:hover {background-color: green}
+
+button:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}  
+    </style>
+    <form><button type="submit">Friends list</button> 
+    </form>
+        <head>
+</html>
