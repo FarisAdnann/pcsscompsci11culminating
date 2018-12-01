cada992672046deae40d3a85fd997f80744e4228
farisadnan02 <44843753+farisadnan02@users.noreply.github.com>
2018-11-30 19:17:54 -0500

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -10,22 +10,17 @@ input[type=text], input[type=password] {
     background: #f1f1f1;
 }
 button {
-    background-color: green;
+    background-color: #4CAF50;
     color: white;
     padding: 14px 20px;
     margin: 8px 10px;
     border: 1px solid green;
-    -webkit-transition-duration: 0.4s; /* Safari */
-    transition-duration: 0.4s;
     cursor: pointer;
     width: 8%;
 }
 button:hover{
-
-  background-color: #4CAF50;
-    color: white;
-    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
-}    
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
 input[type=text]:focus, input[type=password]:focus {
     background-color: #ddd;
     outline: none;
@@ -35,13 +30,13 @@ input[type=text]:focus, input[type=password]:focus {
 <body>
 <form method="post" style="border:2px solid #ccc" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
 <div class="container">
-<h1> Student Login </h1> 
+<h1> Login Page </h1> 
 <label for="username"><b>Username</b></label><input type="text" placeholder="Enter Username" name="username" required><br>
 <label for="password"><b>Password</b></label><input type="password" name="password" placeholder="Enter Password" required><br>
 <div class="clearfix">
 <button type="submit" name="login" value="Log In"><b>Log In</b>
 <button type="reset" name="erase" value="Erase"><b>Reset</b>
 </div><br>
-<a href="student_registration.php">Sign Up!</a>
+<a href="registration_form.php">Sign Up!</a>
 </div>
 </form>
