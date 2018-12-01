63a71ed1eb98d93e91854699549bbc2bd4183423
farisadnan02 <44843753+farisadnan02@users.noreply.github.com>
2018-11-30 19:19:48 -0500

    Update friends_list_button.php

--- friends_list_button.php
+++ friends_list_button.php
@@ -23,8 +23,31 @@ button:active {
   box-shadow: 0 5px #666;
   transform: translateY(4px);
 }  
+ 
+
+.dropdown-list a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown a:hover {background-color: green;}
+
+.show {display: block;}
     </style>
-    <form><button type="submit">Friends list</button> 
-    </form>
         <head>
+            <body>    
+                
+ <div class="dropdown">
+<button onclick="myFunction()" class="dropbtn">Friends List</button>  
+  <div class="dropdown-list">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+  </div>
+
+               
+</body>
 </html>
