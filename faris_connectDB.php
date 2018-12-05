6ad3a6032b404e2cdf4904a63bc9262ea534fb0f
farisadnan02 <44843753+farisadnan02@users.noreply.github.com>
2018-12-05 06:52:46 -0500

    Add files via upload

---
+++ faris_connectDB.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$dbname = "mygr11compsciculminating";
+
+// Create connection
+$conn = new mysqli($servername, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
---
+++ insert_login.php
@@ -0,0 +1,12 @@
+<?php
+include 'faris_connectDB.php'
+$sql = "INSERT INTO mylogin (username, password)
+VALUES ('$username', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
