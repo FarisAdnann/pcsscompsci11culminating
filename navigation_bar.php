234d44ee0815bfbe1d14bf6a410ce63cfacb7611
Josharis <45038547+Josharis@users.noreply.github.com>
2018-12-06 08:36:32 -0500

    Changed .txt to .php file

---
+++ navigation_bar.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    body {
+    
+}
+
+.sidebar {
+    height: 100%;
+    width: 180px;
+    position: absolute;
+    top: 0;
+     left:0;   
+    font-size: 35px;
+    background-color: lightgrey;
+    overflow-x: auto; 
+       
+}
+   a {
+   line-height: 50px;
+}
+
+.sidebar a {
+    text-decoration: none;
+        position: auto;
+    padding: 0px 0px 31px 30px;
+        display: inline-block;
+    color: maroon;
+    
+}
+
+.sidebar a:hover {
+    color: green;
+padding-left: 20px;    
+background-color: cyan;
+</style>
+</head>
+<body>
+
+<div class="sidebar">
+    <h4>Navigation Help</h4>
+  <a href="#">About</a>
+  <a href="#">Services </a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+  
+</body>
+</html> 
