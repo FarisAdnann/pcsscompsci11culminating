77ca0cc10e630db348116351f4e08b247f93916a
farisadnan02 <44843753+farisadnan02@users.noreply.github.com>
2019-01-16 08:44:41 -0500

    Updated Connection to Database with AwardSpace

--- connectDB.php
+++ connectDB.php
@@ -1,8 +1,11 @@
 <?php
 $servername = 'localhost';
-$username = 'id8418842_root';
+//$username = 'id8418842_root';
 $password = 'josharis123';
-$dbname = 'id8418842_mygr11compsciculminating';
+//$dbname = 'id8418842_mygr11compsciculminating';
+
+//Temporary Database with AwardSpace Until 000WebHost Begins Working Again
+$dbname = '2934836_mygr11compsciculminating';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
