9dcbbe70a2d2c54946f3df45e0411b7626ae38be
farisadnan02 <44843753+farisadnan02@users.noreply.github.com>
2019-01-15 19:31:21 -0500

    Re-Added Database and Added Confirm Password

--- registration_form.php
+++ registration_form.php
@@ -1,3 +1,5 @@
+<?php ob_start(); ?>
+<html>
 <style>
 body {font-family: sans-serif;}
 * {box-sizing: border-box;}
@@ -26,38 +28,50 @@ input[type=text]:focus, input[type=password]:focus {
     outline: none;
 }
 .container {
-    padding: 16px;
+  padding: 16px;
 }
 </style>
 <body>
-<form method="post" style="border:2px solid #ccc" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
+<form method="post" style="border:2px solid #ccc" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
 <div class="container">
 <h1> Registration Page </h1>
 <p> Please fill out this sign up form to create an account. </p>
 <label for="username"><b>Username</b></label>
-<input type="text" name="username" placeholder="Enter Username" pattern=".{6,16}" required title="6 to 16 characters"><br>
+<input type="text" name="user_name" placeholder="Enter Username" pattern=".{6,16}" required title="6 to 16 characters"><br>
 <label for="password"><b>Password</b></label>
-<input type="password" name="password" placeholder="Enter Password" pattern="(?=.*\d).{8,16}" required title="Must contain at least one number and be within 8 to 16 characters"><br>
+<input type="password" name="pass_word" placeholder="Enter Password" pattern="(?=.*\d).{8,16}" required title="Must contain at least one number and be within 8 to 16 characters"><br>
+<label for="confirmpassword"><b>Confirm Password</b></label>
+<input type="password" name="confirmpassword" placeholder="Re-Enter Password" pattern="(?=.*\d).{8,16}" required title="Must contain at least one number and be within 8 to 16 characters"><br>
 <div>
-<button type="submit" name="register" value="Register"><b>Register</b>
-<button type="reset" name="erase" value="Erase"><b>Reset</b>
+<button type="submit" name="register"><b>Register</b>
 </div>
 <br>
 <a href="login_form.php">Return back to Login!</a>
 </div>
 </form>
 <?php
+$username = $password = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$username = test_input($_POST["username"]);
-$password = test_input($_POST["password"]);
-if (isset($_POST['register'])) header('Location: test.php'); include 'insert_login.php';
-}
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+  $username = $_POST["user_name"];
+  $password = $_POST["pass_word"];
+  $confirmpassword = $_POST["confirmpassword"];
+  if (isset($_POST['register'])) {
+    if ($password == $confirmpassword) {
+      include 'connectDB.php';
+      $sql = "INSERT INTO myLogin (username, password)
+      VALUES ('$username', '$password')";
+      if ($conn->query($sql) === TRUE) {
+        header ('Location: login_form.php');
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+      $conn->close();  
+    } else {
+        "Passwords Do Not Match... Try Again!";
+    }
+  }
 }
 ?>
 </body>
 </html>
+<?php ob_end_flush(); ?>
